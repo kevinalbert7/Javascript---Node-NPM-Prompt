133781e649d68ab70fb0eb6d9ecf36e4c2f1f134
kevinalbert7 <20ko@live.fr>
2022-04-08 21:02:08 +0200

    multiply

--- multiply.js
+++ multiply.js
@@ -4,7 +4,6 @@
 // - Créez une fonction `multiply` qui recevra un paramètre (un nombre entier)
 // - Faites en sorte que la fonction affiche la table de multiplication de ce nombre dans la console (de 1 à 10) ligne par ligne
 // - Vous devrez donner l'argument dans le terminal en appelant le programme
-    
 //     ⇒ Si vous ne donnez pas d'argument, le programme doit afficher "error" dans la console
 
 const multiply = (n) => {
